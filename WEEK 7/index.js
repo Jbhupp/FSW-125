960a694286e8a0005871908fbdf41938e0da7cba
github_Jbhupp <jenni.hupp@bryanuniversity.edu>
2022-11-12 14:17:28 -0500

    commit

---
+++ WEEK 7/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+app.use("/events", require("./capstone/routes/trackEvents.js"));
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({errMsg: err.message})
+})
+
+app.listen(3000, () => {
+    console.log("The server is running on Port 3000.")
+});
+
